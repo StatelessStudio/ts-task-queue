3ec3a113660b112a729f23cc0c0f28a4cfb2cf93
Drew Immerman <drewimmerman@gmail.com>
2022-10-03 14:50:01 -0400

    Removed app title from env

--- script/example.ts
+++ script/example.ts
@@ -5,7 +5,6 @@ import { env } from '../src/environment';
  * Do something!
  */
 export async function example(): Promise<void> {
-	console.log('App Name: ', env.APP_TITLE);
 	console.log('Environment: ', env.NODE_ENV);
 }
 
